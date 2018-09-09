21afe758228e0118e8143e9354cec89d4ad8a480
Eelke van Turnhout <eelketurnhout3@gmail.com>
2018-09-09 22:09:47 +0200

    Added client classes for easy use in other projects

---
+++ src/AbstractClient.php
@@ -0,0 +1,54 @@
+<?php
+namespace Evt\Util;
+
+/**
+ * AbstractClient
+ *
+ * Class that with base client functions
+ *
+ * @author Eelke van Turnhout <eelketurnhout3@gmail.com>
+ * @version 1.0
+ */
+abstract class AbstractClient
+{
+
+    /**
+     * Set the configurations
+     *
+     * @param Evt\Util\Client\AbstractConfig $config The configurations with credentials
+     */
+    public function setConfig(Config $config)
+    {
+        $this->config = $config;
+    }
+
+    /**
+     * Get the configurations
+     *
+     * @return Evt\Util\Client\AbstractConfig
+     */
+    public function getConfig()
+    {
+        return $this->config;
+    }
+
+    /**
+     * Connect to the server
+     */
+    abstract public function connect();
+
+    /**
+     * Disconnect from the server
+     */
+    abstract public function disconnect();
+
+    /**
+     * Login to the server
+     */
+    abstract public function login();
+
+    /**
+     * Logout from the server
+     */
+    abstract public function logout();
+}
